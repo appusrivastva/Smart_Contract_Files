a3e02f43b083710b328fcc39064ac8fa3d1e1d4f
Apoorva srivastva <140163599+appusrivastva@users.noreply.github.com>
2025-05-20 18:25:11 +0530

    Update ChainBoost.sol

--- MLM/1-Level MLM System/ChainBoost.sol
+++ MLM/1-Level MLM System/ChainBoost.sol
@@ -1,75 +1,150 @@
 //SPDX-License-Identifier:GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+
 interface IERC20 {
-    function transfer(address receipient,uint amount) external returns (bool success);
-    function transferFrom(address sender,address receipient,uint amount) external returns (bool success); 
-    
+    function transfer(address receipient, uint256 amount)
+        external
+        returns (bool success);
+
+    function transferFrom(
+        address sender,
+        address receipient,
+        uint256 amount
+    ) external returns (bool success);
 }
 
+// a basic 1-level MLM registration with referral rewards paid in BST tokens.
+
+
 contract ChainBoost {
-    // 1-Level MLM System
-    IERC20 public  BST;
+      IERC20 public BST;
     address public owner;
+    bool public  pause;
 
-    uint public  constant REGISTRATION_FEE=10*10**18;   //10 bst per registration  
+    uint256 public  REGISTRATION_FEE = 10 * 10**18; //10 bst per registration
+
     constructor(address _bstAddress) {
-        BST=IERC20(_bstAddress);
-        owner=msg.sender;
+        BST = IERC20(_bstAddress);
+        owner = msg.sender;
+    }
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner");
+        _;
+    }
+    modifier  whenNotPaused{
+        require(!pause, "ChainBoost is Paused");
+        _;
     }
 
-    struct User{
-        address referrer;  //address of referral
-        uint referralIncome;  //total direct referralIncome
-        bool isRegistered;   //user -> already register-> true
-        uint referralCount;   // total direct referralCount
-        uint registrationTime;  //time of registration
+    struct User {
+        address referrer; //address of referral
+        uint256 referralIncome; //total direct referralIncome
+        bool isRegistered; //user -> already register-> true
+        uint256 referralCount; // total direct referralCount
+        uint256 registrationTime; //time of registration
         address[] directReferrals; //address of all my referral
-
     }
-    mapping (address => User ) public users;
+    mapping(address => User) public users;
 
-    function  registration(address _referrer) external {
-        require(!users[msg.sender].isRegistered,"Already registered");
-        require(_referrer!=address(0),"invalid referral");
+    function registration(address _referrer) external whenNotPaused{
+        require(!users[msg.sender].isRegistered, "Already registered");
+        require(_referrer != address(0), "invalid referral");
 
+        require(
+            users[_referrer].isRegistered,
+            "Referral user did not complete any registration "
+        );
 
-        require(users[_referrer].isRegistered,"Referral user did not complete any registration ");
+        require(
+            BST.transferFrom(msg.sender, address(this), REGISTRATION_FEE),
+            "registration failed"
+        );
 
-
-        require(BST.transferFrom(msg.sender,address(this), REGISTRATION_FEE),"registration failed");
-
-
-        // distribute 40% to direct refferal 
+        // distribute 40% to direct refferal
         // calculate reward and send to referrer address
-        uint reward=calculateDirectReferralReward(REGISTRATION_FEE);
+        uint256 reward = calculateDirectReferralReward(REGISTRATION_FEE);
         BST.transfer(_referrer, reward);
 
-
         // update values
-        users[_referrer].referralIncome+=reward;
-        users[_referrer].referralCount+=1;
+        users[_referrer].referralIncome += reward;
+        users[_referrer].referralCount += 1;
         users[_referrer].directReferrals.push(msg.sender);
 
-        users[msg.sender]=User({
-            referrer: _referrer,
-            referralIncome : 0,
-            isRegistered   :true,
-            referralCount     :0,
-            registrationTime  : block.timestamp,
-            directReferrals : new address[](0)
+        users[msg.sender].referrer = _referrer;
+        users[msg.sender].referralIncome = 0;
+        users[msg.sender].isRegistered = true;
+        users[msg.sender].referralCount = 0;
+        users[msg.sender].registrationTime = block.timestamp;
+        // directReferrals array is empty by default
+
+        // Remaining 60% stays in contract or later withdrawn by admin
+    }
+
+    function calculateDirectReferralReward(uint256 amount)
+        internal
+        pure
+        returns (uint256)
+    {
+        return (amount * 40) / 100;
+    }
+
+    function changeRegsiterationFee(uint _percentage) external  onlyOwner{
+        require(_percentage < 99, "invalid percentage");
+        REGISTRATION_FEE = _percentage*10**18;
+    }
+
+    function paused() external onlyOwner{
+        pause=true;
+    }
+    function unpaused() external onlyOwner{
+        pause=false;
+    }
+
+    // withdraw token by user
+    function withdraw(uint amount) external onlyOwner{
+        BST.transfer(owner, amount);
+    }
+
+    //transfer ownership
+
+    function transferOwnership(address _newOwner) external onlyOwner{
+        require(_newOwner != address(0), "Invalid new owner");
+        owner = _newOwner;
+    }
+
+    //  Get All Direct Referrals of a User
+    function getDirectReferrals(address _user ) external view returns(address[] memory){
+          return users[_user].directReferrals;
+    }
+    
+
+    // Get Basic Info of a User
+
+    function getBasicInfoUser(address user) external view returns(address referrer, 
+    uint referralIncome,bool isRegistered,uint referralCount,uint registrationTime) {
+        User storage u=users[user];
+        return (
+            u.referrer,
+            u.referralIncome,
+            u.isRegistered,
+            u.referralCount,
+            u.registrationTime
         
+        );
+    }
+    
 
-        });
-         // Remaining 60% stays in contract or later withdrawn by admin
+    // Update BST Token Address (in case token address changes)
 
-
+    function updateBSTAddress(address _token) external onlyOwner{
+        require(_token != address(0), "invalid new bst");
+        BST = IERC20(_token);
     }
 
-    function calculateDirectReferralReward(uint amount)  internal   pure returns(uint){
-        return  (amount*40)/100;
 
 
-    }
- 
+
+
+    
 }
